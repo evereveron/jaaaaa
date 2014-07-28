f80a09bdf01b69b6289e17b760534a07b0f1339e
Anji <anjizhao@users.noreply.github.com>
2014-07-28 15:25:17 -0400

    organized coins a bit

--- Food.java
+++ Food.java
@@ -7,7 +7,8 @@ public class Food extends Item {
 	public Food(String name) { 
 		super(name); 
 		description = describe(name); 
-		health = 0; 
+		value = setValue(name); 
+		health = setHealth(name); 
 	}
 
 	public String describe(String name) { 
@@ -18,9 +19,39 @@ public class Food extends Item {
 			String description = "a typical apple."; 
 		} else if (name == "escargot") {
 			String description = "a cooked snail."; 
+		} else {
+			String description = "a food."; 
 		}
-
 		return description; 
 	}
 
+	private int setHealth(String name) {
+		if (name == "pizza") {
+			int health = 5;
+		} else if (name == "apple") { 
+			int health = 10;
+		} else if (name == "escargot") {
+			int health = 12; 
+		} else { 
+			int health = 0; 
+		}
+		return health; 
+	} 
+
+	private int setValue(String name) {
+		if (name == "pizza") {
+			int value = 50; 
+		} else if (name == "apple") {
+			int value = 5; 
+		} else if (name == "escargot") {
+			int value = 100; 
+		} else {
+			int value = 10; 
+		} 
+		return value; 
+	}
+
+	public int getHealth(String name) {
+		return self.health; 
+	}
 }
--- Item.java
+++ Item.java
@@ -4,14 +4,24 @@ public class Item {
 
 	public String name;
 	public String description;
+	private int value; 
 
 	public Item(String name) { 
 		this.name = name;
 		description = describe(name);
+		value = setValue(name); 
 	}
 
 	public String describe(String name) {
 		return "this is an item."; 
 	}
+
+	public int setValue(String name) {
+		return 1; 
+	}
+
+	public int getValue(String name) {
+		return self.value; 
+	} 
 
 }
